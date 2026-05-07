6f766b5774dd1eb513f264879fa4fb2cad47461e
MostafizFahim <98682669+MostafizFahim@users.noreply.github.com>
2026-05-07 12:39:22 +0600

    Final Fix of Frontend and backend Deployment

--- backend/account-service/src/main/java/com/banking/account/controller/TransactionController.java
+++ backend/account-service/src/main/java/com/banking/account/controller/TransactionController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/transactions")
 @RequiredArgsConstructor
 @Slf4j
 
